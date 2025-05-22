fb513c94f06b454be5cc6457926efa529f6c49f7
kikiepm <kikiemakaka@gmail.com>
2025-05-22 18:48:37 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "EbSparco Bot",
+  botName: "Mutso Bot",
   botOwner: 'Peacey', // Your name
   ownerNumber: '263719553607', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "private",
+  commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.0",
 };
